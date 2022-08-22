3d0d463e1427c758d518faaac98e258e5cbfa94a
Mr.M.V.BALA <mvbalakrishnan95@gmail.com>
2022-08-22 18:50:56 +0530

    it is udemy exercise

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        IntEqualityPrinter.printEqual(2, 2, 2);
+        IntEqualityPrinter.printEqual(-1, 2, 3);
+        IntEqualityPrinter.printEqual(25, 34, 3);
     }
 }
